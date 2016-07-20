380bc1e13f81444a94bae4e2806b374e07d8e653
Laine PP <elainepportela@gmail.com>
2016-07-20 20:44:54 -0300

    atualizar

--- src/main/java/edu/ifma/dai/transportadora/ServletInitializer.java
+++ src/main/java/edu/ifma/dai/transportadora/ServletInitializer.java
@@ -1,13 +1,13 @@
-package edu.ifma.dai.transportadora;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TranspostadoraApplication.class);
-	}
-
-}
+//package edu.ifma.dai.transportadora;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(TranspostadoraApplication.class);
+//	}
+//
+//}
